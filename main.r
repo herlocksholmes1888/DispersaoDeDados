50c6057bf9ab686788af0dce7171db72ba1b5dc4
herlocksholmes1888 <99362386+herlocksholmes1888@users.noreply.github.com>
2023-04-13 17:34:51 +0000

    Initial commit

---
+++ main.r
@@ -0,0 +1,8 @@
+dataset <- c(1, 1, 1, 2, 3, 4, 5, 5, 5, 6, 10, 20, 30, 80, 500, 700)
+
+sqrt(var(dataset)) # square root of variance
+sd(dataset) # standard deviation
+
+png(file="nooutlierboxplot.png")
+boxplot(dataset, col="blue", border="darkblue", outline=FALSE)
+dev.off()
